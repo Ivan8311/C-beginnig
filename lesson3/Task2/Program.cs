7d1c08e573ac58538156658b23f57f20d74087af
Ivan <sokol831@rambler.ru>
2024-02-25 14:10:12 +0200

    add lesson3:Task2

---
+++ lesson3/Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿void printsquares (int limit)
+{
+    int i = 1;
+    while (i <= limit)
+    {
+        Console.Write($"{i * i} ");
+        i++;
+    }
+}
+printsquares(5);
+printsquares(10);
+printsquares(15);
