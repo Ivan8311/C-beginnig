5052b5173c89d0782ce464d7729113eccf7ac402
Ivan <sokol831@rambler.ru>
2024-02-26 19:40:29 +0200

    add Lesson:DZ

---
+++ lesson3/DZ/Program.cs
@@ -0,0 +1,48 @@
+﻿int n = 10;
+int[] arr = new int[n];
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = i + 1;
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+FillArray(arr);
+PrintArray(arr);
+Console.WriteLine();
+
+int GetSumOfElements(int[] arr)
+{
+    int sum = 0;
+    foreach (int e in arr)
+    {
+        sum = sum + e;
+    }
+    Console.WriteLine($"Sum of elements = {sum}");
+    return sum;
+}
+GetSumOfElements(arr);
+
+int GetProductOfElements(int[] arr)
+{
+    int Product = 1;
+    foreach (int e in arr)
+    {
+        Product = Product * e;
+    }
+    Console.WriteLine($"Product of elements = {Product}");
+    return Product;
+}
+GetProductOfElements(arr);
+
+// int product = GetProductOfElements(arr)
+// Console.WriteLine(sum);
+// Console.WriteLine(product);
