7afaec9a7d0f62bb8ef1b265e3f2bf89509a667f
Ivan <sokol831@rambler.ru>
2024-03-06 15:49:12 +0200

    add seminar:Task1

---
+++ seminar5/Task1/Program.cs
@@ -0,0 +1,54 @@
+﻿// Семинар 5. Двумерные массивы
+// 15 мин
+// Задайте двумерный массив. Найдите элементы, у которых оба
+// индекса чётные, и замените эти элементы на их квадраты.
+// Пример
+// 2 3 4 3              4 3 4 3
+// 4 3 4 1 =>           4 3 4 1
+// 2 9 5 4              2 9 25 4
+
+void Print_arr(int[,] mas)
+{
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            Console.Write($"{mas[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Sq(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                array[i, j] = array[i, j] * array[i, j];
+            }
+        }
+    }
+    return array;
+}
+
+Console.Write("Введите размерность m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите размерность n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = new int[m, n];
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = new Random().Next(1, 10);
+    }
+}
+Print_arr(arr);
+Console.WriteLine();
+Print_arr(Sq(arr));
