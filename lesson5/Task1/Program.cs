0a8dcbe5c12d74cb3906a66b9f2767993537ea0a
Ivan <sokol831@rambler.ru>
2024-03-14 14:38:17 +0200

    add lesson5

---
+++ lesson5/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿int Fact(int n) // рекурсия N!
+{
+    if (n == 1 || n == 0)
+    {
+        Console.WriteLine($"Stop: {n}");
+        return 1;
+    }
+    Console.WriteLine(n);
+    return n * Fact(n - 1);
+}
+Console.Write(Fact(5));
+
+// F11 шаг с заходом
+// F10 шаг с обходом
+// F5 продолжитьcd 
